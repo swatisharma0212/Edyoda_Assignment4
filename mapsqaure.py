9b67ef05cabccf6471e4bafbace60423f87f267a
swatisharma0212 <109784479+swatisharma0212@users.noreply.github.com>
2022-08-10 20:43:52 +0530

    Add files via upload

---
+++ mapsqaure.py
@@ -0,0 +1,13 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+nums = (4,5,2,9) 
+
+result = map(lambda x: x *x, nums) 
+
+print(list(result))
